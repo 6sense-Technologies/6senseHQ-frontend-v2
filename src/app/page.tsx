d22fd527d5c7491a7416f67e02e214af009a4f28
hasibmuhammad <hasibmu99@gmail.com>
2024-08-17 16:19:23 +0600

    Services section - added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Companies from "./components/Companies";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
-    <main>
+    <main className="font-plex-sans">
       <Hero />
       <Companies />
+      <Services />
     </main>
   );
 }
