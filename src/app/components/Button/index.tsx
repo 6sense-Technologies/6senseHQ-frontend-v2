221ddd73773c8417e00b1b47efdc360b4fdf079d
hasibmuhammad <hasibmu99@gmail.com>
2024-08-17 10:15:31 +0600

    achievements array of object added for hero

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="w-full lg:w-2/3 bg-secondary text-white font-bold px-5 py-4">
+      Get In Touch
+    </button>
+  );
+};
+
+export default Button;
