0a70ee917f1a12031cba32e1c808eebac1175471
hasibmuhammad <hasibmu99@gmail.com>
2024-08-30 18:12:56 +0600

    replaced appostrophie

--- src/app/components/LeaderShip/index.tsx
+++ src/app/components/LeaderShip/index.tsx
@@ -46,9 +46,9 @@ const LeaderShip = ({
               </div>
               <p className="text-blackSecondary">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                industry. Lorem Ipsum has been the industry&appos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
@@ -74,9 +74,9 @@ const LeaderShip = ({
               </div>
               <p className="text-blackSecondary">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                industry. Lorem Ipsum has been the industry&appos;s standard
+                dummy text ever since the 1500s, when an unknown printer took a
+                galley of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
